62c09780f0cf97bdf0da07104f87668ded87cdfa
briann_cs <brianncsrelease@gmail.com>
2017-02-06 17:26:42 -0300

    Purenexus-ify

--- aosp_hero2lte.mk
+++ aosp_hero2lte.mk
@@ -14,7 +14,7 @@
 #
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
+$(call inherit-product, device/samsung/hero2lte/device.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
 # Inherit from the 64 bit configuration
@@ -23,11 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := aosp_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_PACKAGES += \
+   Launcher3 \
+   Camera2
