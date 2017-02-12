6a729e2f1d799da2af8eaf911c2619ed6bf803bc
briann_cs <brianncsrelease@gmail.com>
2017-02-12 01:09:11 -0300

    Zephyr-ify

---
+++ zos.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/hero2lte/device.mk)
+$(call inherit-product, device/samsung/hero-common/hero-common.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common ZOS stuff.
+$(call inherit-product, vendor/zos/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := zos_hero2lte
+PRODUCT_DEVICE := hero2lte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := Samsung Galaxy S7 Edge
