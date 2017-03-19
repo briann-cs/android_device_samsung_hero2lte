77e6196fda071ab72912f6f3464f4e8bb9f323e4
briann_cs <brianncsrelease@gmail.com>
2017-03-19 06:49:39 +0000

    Added some things

--- cos.mk
+++ cos.mk
@@ -1,17 +1,9 @@
-#
-# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
-# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/hero2lte/device.mk)
@@ -23,11 +15,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common COS stuff.
-$(call inherit-product, vendor/cos/common.mk)
-
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cos_hero2lte
 PRODUCT_DEVICE := hero2lte
+PRODUCT_NAME := cos_hero2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+# Override build properties.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hl3gxx \
+    TARGET_DEVICE=hl3g \
+    BUILD_FINGERPRINT="ro.build.fingerprint=samsung/hero2ltexx/hero2lte:7.0/NRD90M/G935FXXU1DQB7:user/release-keys" \
+    PRIVATE_BUILD_DESC="hero2ltexx-user 7.0 NRD90M G935FXXU1DQB7 release-keys"
