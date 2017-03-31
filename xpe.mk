b9f1e44ecd60f6743b39b69ed093e4fde3a4277b
briann_cs <brianncsrelease@gmail.com>
2017-03-31 20:41:52 -0300

    Xperience-ify

---
+++ xpe.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
+$(call inherit-product, device/samsung/hero-common/hero-common.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common XPe stuff.
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := xpe_hero2lte
+PRODUCT_DEVICE := hero2lte
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
